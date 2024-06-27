75dce8093dcab9d4bf47b1f58dc9eac43fda4ef9
Aymen-Khanfir <aymenkhanfir56@gmail.com>
2024-06-27 17:25:49 +0100

    Adding the form widget along with its correponding child widgets to better controle error validation messages and placing validation logic accross all the app

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 enum Categories { vegetables, fruit, meat, dairy, carbs, sweets, spices, convenience, hygiene, other }
 
 class Category {
-  const Category(this.name, this.color);
+  const Category(this.title, this.color);
 
-  final String name;
+  final String title;
   final Color color;
 
 }
