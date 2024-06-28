f1d8ee661c3dd72fd9a68b5366770ec836e3857b
Aymen-Khanfir <aymenkhanfir56@gmail.com>
2024-06-28 16:59:32 +0100

    Sending a Post request to the backend

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
@@ -24,15 +26,29 @@ class _NewItemState extends State<NewItem> {
   void _saveItem() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      Navigator.of(context).pop(
-        GroceryItem(
-          // It's not a perfect id, juste to speed process
-          id: DateTime.now.toString(),
-          name: _enteredGroceryName,
-          quantity: _enteredGroceryQuantity,
-          category: _selectedCategory,
-        ),
+      final url = Uri.https(
+          'shopping-list-5d7f5-default-rtdb.europe-west1.firebasedatabase.app', "/shopping-list.json");
+      http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode({
+          'name': _enteredGroceryName,
+          'quantity': _enteredGroceryQuantity,
+          'category': _selectedCategory.title,
+        }),
       );
+      
+      
+
+      // Navigator.of(context).pop(
+      //   GroceryItem(
+      //     // It's not a perfect id, juste to speed process
+      //     id: DateTime.now.toString(),
+      //     name: _enteredGroceryName,
+      //     quantity: _enteredGroceryQuantity,
+      //     category: _selectedCategory,
+      //   ),
+      // );
     }
   }
 
